5e2ec6c294730e510a9d1a238e92386878f3e8a6
Guilherme Leme de Matos <100931590+guilhermelemematos@users.noreply.github.com>
2024-06-11 08:20:50 -0300

    Add files via upload

---
+++ aula_api/api/data.php
@@ -0,0 +1,43 @@
+<?php
+    return[
+        [
+            'nome' => 'Zacarias',
+            'email' => 'otaldozacarias@gmail.com'
+        ],
+        [
+            'nome' => 'Antedegmon',
+            'email' => 'otaldoantedegmon@gmail.com'
+        ],
+        [
+            'nome' => 'Goku',
+            'email' => 'otaldoGOKU@gmail.com'
+        ],
+        [
+            'nome' => 'VictorIsGay',
+            'email' => 'victorisgay@gmail.com'
+        ],
+        [
+            'nome' => 'Kim-Jon-Un',
+            'email' => 'coreia.norte@gmail.com'
+        ],    
+        [
+            'nome' => 'Zé Felipe',
+            'email' => 'asminhasmaria@gmail.com'
+        ],    
+        [
+            'nome' => 'Picachu',
+            'email' => 'pika.pika@gmail.com'
+        ],    
+        [
+            'nome' => 'Antônio Viagra',
+            'email' => 'SempreDentro@gmail.com'
+        ],    
+        [
+            'nome' => 'Giovanni',
+            'email' => 'Giovas.dale@gmail.com'
+        ],    
+        [
+            'nome' => 'TalDo',
+            'email' => 'TalDoCara@gmail.com'
+        ],    
+    ];   
--- aula_api/app/status/index.php
+++ aula_api/app/status/index.php
@@ -2,18 +2,9 @@
     //Aqui vai ser o APP
     require_once __DIR__ .'../../../api/config.php';
     require_once __DIR__ .'../../../api/response.php';
-
-    if(API_IS_ACTIVE){
-        echo Response::resposta(200,'sucess',[
-            'version' => API_VERSION,
-            'status' => 'ATIVA'
-        ]);
-    }
-    else{
-        echo Response::resposta(200,'sucess',[
-            'version' => API_VERSION,
-            'status' => 'MANUTENÇÃO'
-        ]);    
-    }
-
+    
+    echo Response::resposta(200,'sucess',[
+        'version' => API_VERSION,
+        'status' => 'MANUTENÇÃO'
+    ]);    
 ?>
