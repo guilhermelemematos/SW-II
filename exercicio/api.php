64e67c910747033cebc98efd9d99e06ac47f3acc
Guilherme Leme de Matos <100931590+guilhermelemematos@users.noreply.github.com>
2024-08-05 10:54:35 -0300

    Add files via upload

--- exercicio/api.php
+++ exercicio/api.php
@@ -1,9 +1,36 @@
 <?php
-
     header('Content-Type:application/json');
     include 'conexao.php';
 
     $metodo = $_SERVER['REQUEST_METHOD'];
-
     echo json_encode($metodo);
+
+    $url = $_SERVER['REQUEST_URI'];
+
+    //echo json_encode($url);
+
+    $path = parse_url($url, PHP_URL_PATH);
+    $path = trim($path,'/');
+
+    //echo json_encode($path);
+
+    $pathparts = explode('/',$path);
+
+    echo json_encode($pathparts[1]);
+
+    $primeira = isset($pathparts[0]) ? $pathparts[0] : '';
+    $segunda = isset($pathparts[1]) ? $pathparts[1] : '';
+    $terceira = isset($pathparts[2]) ? $pathparts[2] : '';
+    $quarta = isset($pathparts[3]) ? $pathparts[3] : '';
+
+    $response = [
+        'metodo' => $metodo,
+        'primeiraParte' => $primeira,
+        'segundaParte' => $segunda,
+        'terceiraParte' => $terceira,
+        'quartaParte' => $quarta
+    ];
+
+    echo json_encode($response);
+
 ?>
